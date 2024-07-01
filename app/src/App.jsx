8df20f510e14e91130738f422e943cb8f7df2472
gabbwhxz <vnvsgabe@gmail.com>
2024-07-01 14:58:20 -0300

    criado tema e adicionado ThemProvider

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 
+import { ThemeProvider } from 'styled-components'
+import theme from './global/theme'
+
 import GlobalStyle from './global/GlobalStyles'
 import Schedule from './pages/Schedule'
 
 export default function App() {
    return (
       <>
-         <Routes>
-            <Route path="/schedule" element={<Schedule />} />
-         </Routes>
-         <GlobalStyle />
+         <ThemeProvider theme={theme}>
+            <Routes>
+               <Route path="/schedule" element={<Schedule />} />
+            </Routes>
+            <GlobalStyle />
+         </ThemeProvider>
       </>
    )
 }
